203d65a7809e7d9975016b5c31d9f0137d9d86a1
mamoru igamoto <stream99990@yahoo.co.jp>
2021-04-29 15:13:36 +0900

    change: Changed target

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ const ogpImage = ''
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
